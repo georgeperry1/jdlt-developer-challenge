d5275fe66f2a1cc665d82922a9eee2e11010ac71
georgeperry1 <gmperry18@gmail.com>
2018-04-16 15:42:34 +0100

    chore: connected AppHeader to App

--- src/app/components/App/App.js
+++ src/app/components/App/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+import AppHeader from '../AppHeader';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h3>Hello</h3>
+        <AppHeader />
+        <h3>App</h3>
       </div>
     )
   }
