24c7eff24bfb8747a61aab23289c9b4629d930fe
georgeperry1 <gmperry18@gmail.com>
2018-04-16 15:17:25 +0100

    feat: created and configured webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: ['babel-polyfill', './src/app/index.js'],
+  output: {
+    path: './dist/app/',
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015', 'stage-2']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: ['file-loader']
+      },
+    ]
+  }
+}
+
+module.exports = config;
