e08e6769ed69feb755a27c54a69e04271aa30db7
georgeperry1 <gmperry18@gmail.com>
2018-04-16 15:35:52 +0100

    feat: created AppHeader component

---
+++ src/app/components/AppHeader/AppHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './AppHeader.css';
+
+class AppHeader extends Component {
+  render() {
+    return (
+      <div className="app">
+        <h3>Header</h3>
+      </div>
+    )
+  }
+}
+
+export default AppHeader;
