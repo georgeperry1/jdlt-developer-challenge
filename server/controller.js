fc546c9b0880407537c13a5052b77f529506f080
georgeperry1 <gmperry18@gmail.com>
2018-04-17 17:43:00 +0100

    feat: created addSupplier controller

--- server/controller.js
+++ server/controller.js
@@ -22,6 +22,39 @@ module.exports.getSuppliers = async (ctx, next) => {
 
 module.exports.addSupplier = async (ctx, next) => {
   if ('POST' != ctx.method) return await next();
+  try {
+    if (!ctx.request.body.name) {
+      ctx.status = 404
+      ctx.body = {
+        errors:[
+          'Supplier name cannot be empty!'
+        ]
+      };
+      return await next();
+    }
+    // Create new Supplier
+    const supplier = await Supplier.create({
+      name: ctx.request.body.name,
+    });
+    // Create products added with Supplier
+    const promises = await ctx.request.body.products.map(product => Product.create({
+      name: product.name,
+      price: product.price
+    }));
+    // Map product IDs to supplier
+    const products = await Promise.all(promises);
+    supplier.products = products.map(product => product._id);
+    await supplier.save();
+    //Return supplier
+    ctx.body = supplier;
+    ctx.status = 201;
+  }
+  catch (error) {
+    if (error) {
+      ctx.body = error;
+      ctx.status = 400;
+    }
+  }
 }
 
 module.exports.addProduct = async (ctx, next) => {
