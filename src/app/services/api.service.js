57f7b28e73642f5440f76d0f88b7e4521c5c14ab
georgeperry1 <gmperry18@gmail.com>
2018-04-18 15:39:22 +0100

    chore: added fetch API to api.service

--- src/app/services/api.service.js
+++ src/app/services/api.service.js
@@ -1,8 +1,79 @@
 import * as types from '../store/actionTypes';
 
 const API_ROOT = 'http://localhost:3000';
+const HEADER = {
+  'Content-Type': 'application/json'
+};
 
 const apiService = store => next => action => {
+  let newAction;
+
+  // Get Suppliers
+  if (action === types.GET_SUPPLIERS) {
+    fetch(API_ROOT + action.meta.params)
+    .then(response => response.json())
+    .then(fetchedSuppliers => {
+      if (fetchedSuppliers) {
+        newAction = {
+          ...action,
+          type: types.GET_SUPPLIERS_SUCCESS,
+          suppliers: fetchedSuppliers,
+        }
+        store.dispatch(newAction);
+      } else {
+        newAction = {
+          ...action,
+          type: types.GET_SUPPLIERS_FAILURE,
+        }
+        store.dispatch(newAction);
+      }
+    })
+  }
+
+  // Add Supplier
+  if (action === types.ADD_SUPPLIERS) {
+    fetch(API_ROOT + action.meta.params, {
+      method: action.meta.method,
+      header: HEADER,
+      body: JSON.stringify(action.supplier),
+    })
+    .then()
+    .then()
+    newAction = {
+      ...action,
+    }
+    store.dispatch(newAction);
+  }
+
+  if (action === types.DELETE_SUPPLIERS) {
+    fetch()
+    .then()
+    .then()
+    newAction = {
+      ...action,
+    }
+    store.dispatch(newAction);
+  }
+
+  if (action === types.ADD_PRODUCTS) {
+    fetch()
+    .then()
+    .then()
+    newAction = {
+      ...action,
+    }
+    store.dispatch(newAction);
+  }
+
+  if (action === types.DELETE_PRODUCTS) {
+    fetch()
+    .then()
+    .then()
+    newAction = {
+      ...action,
+    }
+    store.dispatch(newAction);
+  }
   return next(action);
 }
 
