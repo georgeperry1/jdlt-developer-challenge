44144d46d15b3b693868c33ab20c3c1f503d957a
georgeperry1 <gmperry18@gmail.com>
2018-04-22 16:09:56 +0100

    chore: added delete function to SupplierListitem

--- src/app/components/SuppliersListItem/SuppliersListItem.js
+++ src/app/components/SuppliersListItem/SuppliersListItem.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
+import * as actions from '../../store/actions';
 
 import './SuppliersListItem.css';
 
-const SuppliersListItem = ({ supplier }) => (
-  <div>
-    <p>{supplier.name}</p>
-  </div>
-);
+class SuppliersListItem extends Component {
+  handleClick = () => {
+    const { supplier } = this.props;
+    this.props.deleteSupplier(supplier._id);
+  }
+  render() {
+    const  { supplier } = this.props;
+    return (
+      <div>
+        <p>{supplier.name}</p>
+        <button onClick={this.handleClick}>X</button>
+      </div>
+    )
+  }
+};
 
 SuppliersListItem.propTypes = {
   supplier: PropTypes.object,
+  deleteSupplier: PropTypes.func,
 };
 
-export default SuppliersListItem;
+const mapDispatchToProps = dispatch => ({
+  deleteSupplier: (supplierId) => dispatch(actions.deleteSupplier(supplierId))
+})
+
+export default connect(null, mapDispatchToProps)(SuppliersListItem);
