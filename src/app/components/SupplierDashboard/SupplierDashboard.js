c84b6ebfbb64251f6d0c10b03c0a61bd46be01bb
georgeperry1 <gmperry18@gmail.com>
2018-04-25 22:50:24 +0100

    feat: added Supplier Details to Dashboard

--- src/app/components/SupplierDashboard/SupplierDashboard.js
+++ src/app/components/SupplierDashboard/SupplierDashboard.js
@@ -9,13 +9,51 @@ import './SupplierDashboard.css';
 
 class SupplierDashboard extends Component {
 
+  renderSelectedSupplier = () => {
+    const { suppliers } = this.props;
+    const { selected } = this.props;
+    const selectedSupplier = suppliers.filter(supplier => {
+      return supplier._id === selected;
+    });
+    console.log('Selected:', selectedSupplier[0]);
+    return (
+      <div>
+        {selected !== '' ?
+          <div className="supplier-dashboard-details-container">
+            <img
+              className="supplier-dashboard-details-image"
+              src={require('../../assets/images/factory.png')}
+              alt="Supplier Logo"
+            />
+            <div className="supplier-dashboard-details-info">
+              <p className="supplier-dashboard-details-header">{selectedSupplier[0].name}</p>
+              <div className="supplier-dashboard-details-divider"></div>
+              <p className="supplier-dashboard-details-description">{selectedSupplier[0].description}</p>
+              <p className="supplier-dashboard-details-header">Number of Products:</p>
+              <div className="supplier-dashboard-details-divider"></div>
+              <p className="supplier-dashboard-details-description">{selectedSupplier[0].products.length}</p>
+            </div>
+          </div>
+        :
+        <div>
+          <p>Please selected a supplier</p>
+        </div>}
+      </div>
+    )
+  }
+
   renderLoading = () => <Loader />;
 
   render() {
-    const { isFetching } = this.props;
+    const { selected } = this.props;
     return (
       <div className="supplier-dashboard-container">
-        
+        <div className="supplier-dashboard-details">
+          {this.renderSelectedSupplier()}
+        </div>
+        <div className="supplier-dashboard-products">
+
+        </div>
       </div>
     );
   }
@@ -29,6 +67,7 @@ SupplierDashboard.propTypes = {
 const mapStateToProps = state => ({
   suppliers: state.suppliers,
   isFetching: state.isFetching,
+  selected: state.selected,
 });
 
 export default connect(mapStateToProps, null)(SupplierDashboard);
