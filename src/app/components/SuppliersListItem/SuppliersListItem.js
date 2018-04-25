d606c493ca0a22a40344623e35baf59b5c581f87
georgeperry1 <gmperry18@gmail.com>
2018-04-25 21:25:21 +0100

    feat: added select action to SupplierListItem

--- src/app/components/SuppliersListItem/SuppliersListItem.js
+++ src/app/components/SuppliersListItem/SuppliersListItem.js
@@ -9,19 +9,25 @@ import './SuppliersListItem.css';
 class SuppliersListItem extends Component {
   handleClick = () => {
     const { supplier } = this.props;
+    this.props.selectSupplier(supplier._id);
+  }
+
+  handleDelete = () => {
+    const { supplier } = this.props;
     this.props.deleteSupplier(supplier._id);
   }
+
   render() {
     const  { supplier } = this.props;
     return (
-      <div className="supplier-item bobble">
+      <div className="supplier-item bobble" onClick={this.handleClick}>
         <img
           className="supplier-item-image"
           src={require('../../assets/images/factory.png')}
           alt="Supplier Logo"
         />
         <p className="supplier-item-name">{supplier.name}</p>
-        <button className="supplier-item-button" onClick={this.handleClick}>
+        <button className="supplier-item-button" onClick={this.handleDelete}>
           <img
             className="supplier-item-delete"
             src={require('../../assets/images/error.png')}
@@ -36,10 +42,12 @@ class SuppliersListItem extends Component {
 SuppliersListItem.propTypes = {
   supplier: PropTypes.object,
   deleteSupplier: PropTypes.func,
+  selectSupplier: PropTypes.func,
 };
 
 const mapDispatchToProps = dispatch => ({
-  deleteSupplier: (supplierId) => dispatch(actions.deleteSupplier(supplierId))
+  deleteSupplier: (supplierId) => dispatch(actions.deleteSupplier(supplierId)),
+  selectSupplier: (supplierId) => dispatch(actions.deleteSupplier(supplierId))
 })
 
 export default connect(null, mapDispatchToProps)(SuppliersListItem);
