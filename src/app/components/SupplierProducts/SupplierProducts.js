a3329f4222f9f1df35a9f1239b0fd391861e9406
georgeperry1 <gmperry18@gmail.com>
2018-04-26 11:19:28 +0100

    refactor: connected SupplierProducts component to redux store and changed to use React Component

--- src/app/components/SupplierProducts/SupplierProducts.js
+++ src/app/components/SupplierProducts/SupplierProducts.js
@@ -1,16 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import './SupplierProducts.css';
 
-const SupplierProducts = ({ supplier }) => (
-  <div className="supplier-products-container">
-    <p>Products:</p>
-  </div>
-);
+class SupplierProducts extends Component {
+
+  renderProducts = (supplier) => {
+    const { products } = this.props;
+    const supplierProducts = supplier.products.map(productId => {
+      const product = products[productId];
+      return (
+        <tr>
+          <td>{product.name}</td>
+          <td>{product.price}</td>
+        </tr>
+      )
+    });
+  }
+
+  render() {
+    const { supplier } = this.props;
+    return (
+      <div className="supplier-products-container">
+        {supplier.products.length ?
+          <div>
+            <p>Products:</p>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <th>Price</th>
+                </tr>
+                {this.renderProducts(supplier)}
+              </tbody>
+            </table>
+          </div>
+        :
+        <div>
+          <p>Supplier has no products</p>
+        </div>
+        }
+      </div>
+    )
+  }
+}
 
 SupplierProducts.propTypes = {
   supplier: PropTypes.object,
+  products: PropTypes.array,
 };
 
-export default SupplierProducts;
+const mapStateToProps = state => ({
+  products: state.products,
+});git
+
+export default connect(mapStateToProps, null)(SupplierProducts);
